11bad52e59f352ba2b27cdf32e4e151395b3fd65
Vitali Khd <hudenkovitalij@gmail.com>
2020-10-19 19:40:09 +0300

    init: add decision

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultStr = '',
+		morseStr = '';
+
+		for (let i = 0; i < expr.length - 9; i += 10) {
+			let str = expr.slice(i, i+10);
+			if (str === '**********') resultStr += ' ';
+			else {
+				for (let j = 0; j < str.length; j +=2) {
+					if (str.slice(j, j+2) === '10') morseStr += '.';
+					else if (str.slice(j, j+2) === '11') morseStr += '-';
+				}
+
+				resultStr += MORSE_TABLE[morseStr];
+				morseStr = '';
+			}
+		}
+    return resultStr;
 }
 
 module.exports = {
